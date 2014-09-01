c6f4654c6003e0b43c38613ffd150097cfb7b470
Steve Patterson <spatte21@btinternet.com>
2014-09-01 10:23:59 +0100

    Added release event endpoint

--- routes/releaseEvent.js
+++ routes/releaseEvent.js
@@ -1,23 +1,35 @@
 var express = require('express'),
   router = express.Router(),
   moment = require('moment'),
-  ReleaseEvent = require('../models/releaseEvent');
+  ReleaseEvent = require('../models/releaseEvent'),
+  Candidate = require('../models/candidate');
 
 // GET /
 router.get('/', function(req, res) {
-  var query = ReleaseEvent.find();
+  var query = ReleaseEvent
+    .find()
+    .where('ends').gt(Date(moment().add('d', -14).toISOString()))
+    .sort({starts: 'asc'});
 
-  if (req.query.status) {
-    query = query.where('status').equalsl(req.query.status);
-  }
 
-  if (req.query.top) {
-    query = query.limit(req.query.top);
-  }
+  // if (req.query.status) {
+  //   query = query.where('status').equalsl(req.query.status);
+  // }
 
-  query
-    .sort('-buildDate')
-    .exec(function(err, results) {
-      res.status(err ? 500 : 200).send(results);
-    });
+    // if (req.query.top) {
+    //   query = query
+    //     .where('ends').gt(Date(moment().add('d', -14).toISOString()))
+    //     .sort('-ends')
+    //     .limit(req.query.top)
+    // }  
+
+    query
+      .exec(function(err, results) {
+        if (err) {
+          console.log('!!' + err);
+        }
+        res.status(err ? 500 : 200).send(results);
+      });
 });
+
+module.exports = router;
